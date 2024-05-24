99e9e8b1e44e7632eb9b8a876ffc7f0ed16b70a6
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-05-24 11:02:22 +0100

    demo app1

--- 5423 - Acesso móvel a sistemas de informação/android/app/src/main/java/com/example/app1/MainActivity.kt
+++ 5423 - Acesso móvel a sistemas de informação/android/app/src/main/java/com/example/app1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.app1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +11,40 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tvMainFoo: TextView
+
+    private lateinit var tvMainHeader: TextView
+
+
+    private lateinit var etMainNome: EditText
+    private lateinit var etMainIdade: EditText
+    private lateinit var tvMainMsg: TextView
+
+    private lateinit var btnMainSubmit: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tvMainHeader = findViewById(R.id.tvMainHeader)
+        btnMainSubmit = findViewById(R.id.btnMainSubmit)
+        tvMainMsg = findViewById(R.id.tvMainMsg)
+        etMainNome  = findViewById(R.id.etMainNome)
+        etMainIdade  = findViewById(R.id.etMainIdade)
+
+
+
+        tvMainHeader.text = "Novo Texto"
+
+        btnMainSubmit.setOnClickListener {
+            var nome = "Anonimo"
+
+            if (etMainNome.text.isNotEmpty()){
+                nome = etMainNome.text.toString()
+            }
+
+            tvMainMsg.text = "Ola $nome, com ${etMainIdade.text}"
+        }
 
 
 
